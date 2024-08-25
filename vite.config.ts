f4d13d77078e30deb949e3eba4270fe081c0c339
harrytran998 <nhattq.coding@gmail.com>
2024-08-25 17:58:15 +0700

    feat: update tsconfig + add ai dép

--- vite.config.ts
+++ vite.config.ts
@@ -1,62 +1,74 @@
 /// <reference types="vitest" />
 
-
-import { fileURLToPath, URL } from 'node:url'
 import viteVueRouter from 'unplugin-vue-router/vite'
 import viteAutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import viteComponents from 'unplugin-vue-components/vite'
 import viteCssTailwind from 'tailwindcss'
 import viteCssAutoPrefixer from 'autoprefixer'
+import { visualizer as viteVisualizer } from "rollup-plugin-visualizer";
+import viteCompress from "vite-plugin-compression2";
+
 
 import { defineConfig, loadEnv } from 'vite'
 import viteVue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 
 export default defineConfig(({ mode }) => {
   const env = loadViteEnv(mode);
+  const isProd = mode === "production";
+
+  const plugins = [
+    viteVueRouter(),
+    viteComponents({
+      dirs: ['src/shared/components'],
+    }),
+    viteAutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      imports: [
+        'vue',
+        VueRouterAutoImports,
+        {
+          pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate']
+        }
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+      dirs: ['src/stores']
+    }),
+    viteVue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon')
+        }
+      }
+    }),
+  ]
+
+  if (isProd) {
+    plugins.push(
+      viteCompress({
+        algorithm: "brotliCompress",
+        deleteOriginalAssets: false,
+        exclude: [/\.(png|avif|webp|jpe?g|gif)$/i, /\.map$/, /\.br$/],
+      }),
+      viteVisualizer({ template: "sunburst" }),
+    );
+  }
+
 
   return {
     define: {
       "process.env": JSON.stringify(env),
     },
-    plugins: [
-      viteVueRouter(),
-      viteComponents({
-        dirs: ['src/shared/components'],
-      }),
-      viteAutoImport({
-        include: [
-          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-          /\.vue$/,
-          /\.vue\?vue/, // .vue
-        ],
-        imports: [
-          'vue',
-          VueRouterAutoImports,
-          {
-            pinia: ['defineStore', 'storeToRefs', 'acceptHMRUpdate']
-          }
-        ],
-        dts: true,
-        viteOptimizeDeps: true,
-        dirs: ['src/stores']
-      }),
-      viteVue({
-        template: {
-          compilerOptions: {
-            isCustomElement: (element) => element.startsWith('iconify-icon')
-          }
-        }
-      })
-    ],
+    plugins,
     css: {
       postcss: {
         plugins: [viteCssTailwind(), viteCssAutoPrefixer()]
-      }
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
     test: {
@@ -66,7 +78,15 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: +(process.env.PORT || '') || 3000,
+    },
+    build: {
+      cssMinify: "lightningcss",
+    },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
   }
 })
 
